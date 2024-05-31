43b99f20da1e1971269b6bdf9b3917bf6ead78ab
Marc <158296221+Axo34080@users.noreply.github.com>
2024-05-31 14:53:27 +0200

    nouveau format image amélioration seo

---
+++ assets/gulpfile.js
@@ -0,0 +1,1 @@
+var gulp=require("gulp"),terser=require("gulp-terser");gulp.task("minify-js",(function(){return gulp.src("*.js").pipe(terser()).pipe(gulp.dest("assets/minified"))})),gulp.task("default",gulp.series("minify-js"));
